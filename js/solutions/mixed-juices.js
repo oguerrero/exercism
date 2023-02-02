4f25666249f9d6be99fc87cb6bb245e25b330efb
oguerrero <oscarguerrero242@gmail.com>
2023-02-02 13:22:53 -0600

    add solution while loops and switch

--- js/solutions/mixed-juices.js
+++ js/solutions/mixed-juices.js
@@ -1,9 +1,3 @@
-// @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
-
 /**
  * Determines how long it takes to prepare a certain juice.
  *
@@ -11,7 +5,11 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+	if (name === "Pure Strawberry Joy") return 0.5
+	if (name === "Energizer" || name === "Green Garden") return 1.5
+	if (name === "Tropical Island") return 3
+	if (name === "All or Nothing") return 5
+	return 2.5
 }
 
 /**
@@ -23,7 +21,16 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+	let limesCutted = 0
+	limes.forEach((lime) => {
+    if (wedgesNeeded > 0) {
+      if (lime === "small") wedgesNeeded -= 6
+      if (lime === "medium") wedgesNeeded -= 8
+      if (lime === "large") wedgesNeeded -= 10
+      limesCutted++
+    }
+	})
+	return limesCutted
 }
 
 /**
@@ -34,5 +41,29 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  let drinksFinished = 0
+	orders.map(order => {
+    if (timeLeft > 0) {
+      timeLeft -= timeToMixJuice(order)
+      drinksFinished++
+    }
+  })
+  return orders.slice(drinksFinished)
 }
+
+// TASK 1
+console.log(timeToMixJuice("Tropical Island"))
+// => 3
+
+console.log(timeToMixJuice("Berries & Lime"))
+// => 2.5
+
+// TASK 2
+console.log(limesToCut(25, ["small", "small", "large", "medium", "small"]))
+// => 4
+
+// TASk 3
+console.log(
+	remainingOrders(5, ["Energizer", "All or Nothing", "Green Garden"]),
+)
+// => ['Green Garden']
