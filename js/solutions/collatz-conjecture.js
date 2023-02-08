c9f13a777eb50ac62b3ef0eedeaa34aadf4f47a4
oguerrero <oscarguerrero242@gmail.com>
2023-02-08 15:21:31 -0600

    add solutions

---
+++ js/solutions/collatz-conjecture.js
@@ -0,0 +1,9 @@
+const steps = (n, count = 0) => {
+  if (n <= 0) throw 'Only positive numbers are allowed'
+
+  if (n === 1) return count
+
+  return steps(n % 2 === 0 ? n / 2 : n * 3 + 1, count + 1)
+}
+
+console.log(steps(-1))
